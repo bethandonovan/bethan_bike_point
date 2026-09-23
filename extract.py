197552dd5dbdde8087efba25d102dc3e5d583228
bethandonovan <bethan.donovan@theinformationlab.co.uk>
2026-09-23 16:07:39 +0100

    committing new code

--- extract.py
+++ extract.py
@@ -2,6 +2,7 @@ import requests as r
 import os as os
 import json as j
 from datetime import datetime
+import time as t
 
 url = 'https://api.tfl.gov.uk/BikePoint/'
 data_dir = 'data' 
@@ -9,12 +10,26 @@ os.makedirs(data_dir, exist_ok=True)
 timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 filename = f'{data_dir}/{timestamp}.json'
 
-response = r.get(url)
+max_retry = 5
+attempt = 0
+delay = 10
 
-status = response.status_code
+while attempt < max_retry:
 
-data = response.json()
+    response = r.get(url)
+    status = response.status_code
 
-with open(filename, 'w') as file: 
-    j.dump(data, file)
+    if 200 <= status < 300:
+        data = response.json()
+        with open(filename, 'w') as file: 
+            j.dump(data, file)
+        print(f'File {filename} was successfully saved')
+        break
 
+    elif  status <200 or status >=500:
+        t.sleep(delay)
+        attempt += 1
+        print(f'Status code: {status}. Retrying. Attempt Number {attempt}')
+
+    else:
+        print(f'Error. Status code {status}. Fix it')
